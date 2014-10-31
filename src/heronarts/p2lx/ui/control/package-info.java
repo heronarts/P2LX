18be9d3f6d87394098d77ce31b4a9d1c2ad51dd8
Mark Slee <mcslee@gmail.com>
2014-10-31 13:28:00 -0700

    Package documentation

---
+++ src/heronarts/p2lx/ui/control/package-info.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2013- Mark C. Slee, Heron Arts LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ##library.name##
+ * ##library.sentence##
+ * ##library.url##
+ *
+ * @author      ##author##
+ * @modified    ##date##
+ * @version     ##library.prettyVersion## (##library.version##)
+ */
+
+/**
+ * Pre-built UI elements for controlling higher-level LX constructs.
+ */
+package heronarts.p2lx.ui.control;
